8d58901e69f5a00c764d248881601ccd74b29c88
Fabio Ribeiro <fabiorphp@gmail.com>
2014-01-05 15:02:13 -0200

    Refactoring and include add and remove segments.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,33 +1,76 @@
-var Main = {};
 
-Main.ready = function() {
+function Main() {
+
+}
+
+Main.prototype.form = function() {
 
     var checkbox  = $('.ui.checkbox'),
-        dropdown  = $('.ui.selection.dropdown');
-        toggle    = $('.ui.toggle.button');
+        dropdown  = $('.ui.selection.dropdown')
+        toggle    = $('.ui.toggle.button')
         buttons   = $('.ui.buttons .button')
-        peek      = $('.peek'),
-        peekItem  = peek.children('.menu').children('a.item'),
-        waypoints = peek.closest('.container').find('h2').first().siblings('h2').addBack();
+        addButton = $('i.add').closest('a.button');
+
+    console.log(addButton);
 
     checkbox.checkbox();
     dropdown.dropdown();
 
-    toggle.state({
+    toggle.filter('.phpmyadmin').state({
         text: {
             active: 'Enabled',
             inactive: 'Disabled'
+        },
+        onActivate: function() {
+            $('input[name=phpmyadmin]').val(1);
+        },
+        onDeactivate: function() {
+            $('input[name=phpmyadmin]').val(0);
         }
     });
 
-    buttons.on('click', function(){
+    toggle.filter('.composer').state({
+        text: {
+            active: 'Enabled',
+            inactive: 'Disabled'
+        },
+        onActivate: function() {
+            $('input[name=composer]').val(1);
+        },
+        onDeactivate: function() {
+            $('input[name=composer]').val(0);
+        }
+    });
+
+    buttons.filter('.phpversion').on('click', function(){
         $(this)
             .addClass('active')
             .addClass('green')
             .siblings()
             .removeClass('active')
             .removeClass('green')
+            .addClass('black');
+
+        $('input[name=php_version]').val($(this)
+            .data('value'));
     });
+
+    addButton.on('click', this.addSegment);
+    this.removePort();
+}
+
+Main.prototype.waypoints = function() {
+
+    var peek      = $('.peek'),
+
+        peekItem  = peek.children('.menu')
+                        .children('a.item'),
+
+        waypoints = peek.closest('.container')
+                        .find('h2')
+                        .first()
+                        .siblings('h2')
+                        .addBack();
 
     waypoints.waypoint({
         continuos: false,
@@ -56,4 +99,118 @@ Main.ready = function() {
     });
 }
 
-$(document).ready(Main.ready);
+Main.prototype.addSegment = function() {
+
+    var segments = $(this).closest('.ui.segment')
+                        .find('.ui.segment');
+
+    var clone = segments.first()
+                    .clone(true, true);
+
+    clone.find('input')
+        .val('');
+
+    segments.last()
+        .after(clone);
+}
+
+Main.prototype.removePort = function() {
+
+    $('a.ui.right.corner').on('click', function() {
+
+        if ($(this).closest('.field > .ui.segment').find('.ui.segment').length === 1) {
+
+            $(this).closest('.ui.segment')
+                .find('input')
+                .val('');
+
+            return;
+        }
+
+        $(this).closest('.ui.segment')
+            .remove();
+    });
+}
+
+Main.prototype.validation = function() {
+
+    var validationRules = {
+        name: {
+            identifier: 'name',
+            rules: [{
+                    type: 'empty',
+                    prompt: 'Please enter VM name'
+                }
+            ]
+        },
+        ipAddress: {
+            identifier: 'ip_address',
+            rules: [{
+                    type: 'empty',
+                    prompt: 'Please enter VM ip address'
+                }
+            ]
+        },
+        memory: {
+            identifier: 'memory',
+            rules: [{
+                    type: 'empty',
+                    prompt: 'Please enter VM memory'
+                }
+            ]
+        },
+        webserverModules: {
+            identifier: 'webserver_modules',
+            rules: [{
+                    type: 'empty',
+                    prompt: 'Please enter Webserver modules'
+                }
+            ]
+        },
+        webserverName: {
+            identifier: 'webserver_name',
+            rules: [{
+                    type: 'empty',
+                    prompt: 'Please enter Webserver name'
+                }
+            ]
+        },
+        webserverDocRoot: {
+            identifier: 'webserver_docroot',
+            rules: [{
+                    type: 'empty',
+                    prompt: 'Please enter Webserver document root'
+                }
+            ]
+        },
+        webserverPort: {
+            identifier: 'webserver_port',
+            rules: [{
+                    type: 'empty',
+                    prompt: 'Please enter Webserver port'
+                }
+            ]
+        },
+        mysqlPass: {
+            identifier: 'myqsl_password',
+            rules: [{
+                    type: 'empty',
+                    prompt: 'Please enter MySQL password'
+                }
+            ]
+        }
+    };
+
+    $('.ui.form').form(validationRules, {
+        inline: true,
+        on: 'blur'
+    });
+}
+
+$(document).ready(function(){
+    var main = new Main();
+
+    main.form();
+    main.validation();
+    main.waypoints();
+});
